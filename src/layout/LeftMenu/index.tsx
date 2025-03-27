0d5059c411cde89cc4dbb3dd4b527965b14e0f22
daisy <8931428+daisyyou@user.noreply.gitee.com>
2025-03-28 01:18:48 +0800

    实现菜单路由跳转

--- src/layout/LeftMenu/index.tsx
+++ src/layout/LeftMenu/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Menu, Spin } from 'antd';
 import { routerArray } from '@/router';
 import { RouteObject } from '@/router/interface';
@@ -20,11 +21,11 @@ const getMenuRoute = (arr: Array<RouteObject>): any => {
     if (item.meta?.isMenu) {
       let children: Array<MenuItem> = [];
       item.children && (children = getMenuRoute(item.children));
-      const { key, title } = item.meta;
+      const { title } = item.meta;
       if (item.children?.length) {
-        result.push(getItem(title, key, null, children));
+        result.push(getItem(title, item.path, null, children));
       } else {
-        result.push(getItem(title, key, null));
+        result.push(getItem(title, item.path, null));
       }
     }
   });
@@ -34,10 +35,29 @@ const getMenuRoute = (arr: Array<RouteObject>): any => {
 const LeftMenu = () => {
   console.log('result', getMenuRoute(routerArray));
   const menuList = getMenuRoute(routerArray);
+  const searchPath = (key: string, routes: RouteObject[] = []) => {
+    let result: RouteObject = {};
+    for (let item of routes) {
+      if (item.path === key) return item;
+      if (item.children) {
+        const res = searchPath(key, item.children);
+        if (Object.keys(res).length) result = res;
+      }
+    }
+    return result;
+  };
+  // 点击当前菜单跳转页面
+  const navigate = useNavigate();
+  const clickMenu: MenuProps['onClick'] = ({ key }: { key: string }) => {
+    // console.log(key);
+
+    // console.log(searchPath(key, routerArray));
+    navigate(key);
+  };
   return (
     <div className="menu">
       <Spin spinning={false} tip="Loading...">
-        <Menu theme="dark" mode="inline" triggerSubMenuAction="click" items={menuList}></Menu>
+        <Menu theme="dark" mode="inline" triggerSubMenuAction="click" items={menuList} onClick={clickMenu}></Menu>
       </Spin>
     </div>
   );
